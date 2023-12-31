f9ea1e4811b2264502de309b7a19f240049e171d
Muzmil Amin <muzammilameen491@gmail.com>
2023-12-31 02:06:27 -0800

    login edit
    login edit

---
+++ backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Notifications\LoginNeedVarifaction;
+
+class LoginController extends Controller
+{
+    //
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'phone' => 'required|numeric|exists:users,phone',
+        ]);
+        $user = User::firstOrCreate([
+            'phone' => $request->phone
+        ]);
+        if (!$user) {
+            return response()->json([
+                'message' => 'Could not process user with that phone number'
+            ]);
+        }
+        $user->notify(new LoginNeedVarifaction());
+        // response
+        return response()->json([
+            'message' => 'Login code sent to ' . $user->phone
+        ]);
+    }
+
+    public function verify(Request $request)
+    {
+        // validate
+        $request->validate([
+            'phone' => 'required|numeric|exists:users,phone',
+            'login_code' => 'required|numeric|between:1111,9999',
+        ]);
+        $user = User::where('phone', $request->phone)->where('login_code', $request->login_code)->first();
+        // is code provided is same as stored
+        // if so return back an auth token
+        if ($user) {
+            $user->update(['login_code' => null]);
+            return $user->createToken($request->login_code)->plainTextToken;
+        }
+
+        // if not return back error
+        return response()->json([
+            'message' => 'Invalid varifaction code'
+        ], 401);
+    }
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -29,13 +25,19 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'login_code',
         'remember_token',
     ];
-    public function driver(){
+    public function routeNotificationFor()
+    {
+        return $this->phone;
+    }
+    public function driver()
+    {
         return $this->hasOne(Driver::class);
     }
-    public function trips(){
+    public function trips()
+    {
         return $this->hasMany(Trip::class);
     }
 }
